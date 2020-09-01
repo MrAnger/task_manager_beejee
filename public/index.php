d9f002a164d27896b9b2ae35c70b64adee4c7e15
MrAnger <slava.sharapov@mail.ru>
2020-09-01 21:50:27 +0700

    Интеграция веб-пака

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ use App\Core\App;
 
 define('ROOT_PATH', realpath(__DIR__ . '/..'));
 define('CONTROLLER_PATH', realpath(ROOT_PATH . '/app/Controllers'));
-define('VIEW_PATH', realpath(ROOT_PATH . '/app/Views'));
-define('LAYOUT_PATH', realpath(VIEW_PATH . '/Layouts'));
+define('VIEW_PATH', realpath(ROOT_PATH . '/resources/views'));
+define('LAYOUT_PATH', realpath(VIEW_PATH . '/layouts'));
 
 require __DIR__ . '/../vendor/autoload.php';
 
--- resources/views/layouts/main.php
+++ resources/views/layouts/main.php
@@ -10,8 +10,12 @@
 	<meta charset="utf-8">
 	<title></title>
 	<meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
-<?= $body ?>
+<div id="app">
+	<?= $body ?>
+</div>
+<script src="js/app.js"></script>
 </body>
 </html>
